9cf0f59c13d056b62f42352d71ced9db8cbdb3b5
xface2face <38925839+xface2face@users.noreply.github.com>
2023-03-30 22:33:38 -0300

    itens selecionaveis

--- script/scripts.js
+++ script/scripts.js
@@ -1,9 +1,23 @@
-function finishOrder() {
-    if (
-        foodName !== undefined &&
-        drinkName !== undefined &&
-        dessertName !== undefined
-    ) {
-        document.querySelector;
+function selectFood(x) {
+    const pick = document.querySelector(".food .selected");
+    if (pick) {
+        pick.classList.remove("selected");
     }
+    x.classList.add("selected");
+}
+
+function selectDrink(y) {
+    const pick = document.querySelector(".drink .selected");
+    if (pick) {
+        pick.classList.remove("selected");
+    }
+    y.classList.add("selected");
+}
+
+function selectDessert(z) {
+    const pick = document.querySelector(".dessert .selected");
+    if (pick) {
+        pick.classList.remove("selected");
+    }
+    z.classList.add("selected");
 }
